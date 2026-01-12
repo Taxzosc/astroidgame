e9532ccdae08d003d8ad166d5f0057a9d34b7823
Taxzosc <martinfh9@gmail.com>
2026-01-12 17:32:36 +0100

    added forward and backward movement with rotation. added updatable and drawable into groupings

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while True:
         log_state()
@@ -18,8 +21,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        updatable.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
         pygame.display.flip()
         the_delta_time = clock.tick(60)
         dt = the_delta_time / 1000
