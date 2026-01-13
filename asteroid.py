1a227344709ddb6b733413b6614cee119c1937e7
Taxzosc <martinfh9@gmail.com>
2026-01-13 14:48:08 +0100

    added a way to kill and split the asteroid

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
+import random
 import pygame
-from constants import LINE_WIDTH
+from logger import log_event
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 from circleshape import CircleShape
 
 class Asteroid(CircleShape):
@@ -11,3 +13,18 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+    
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        if self.radius > ASTEROID_MIN_RADIUS:
+            log_event("asteroid_split")
+            new_angle = random.uniform(20, 50)
+            new_direction = self.velocity.rotate(new_angle)#first smaller asteroid direction
+            new_negative_direction = self.velocity.rotate(-new_angle)#2nd smaller asteroid direction at the oposite angle
+            new_asteroid_radius = self.radius - ASTEROID_MIN_RADIUS
+            asteroid_one = Asteroid(self.position.x, self.position.y, new_asteroid_radius)
+            asteroid_two = Asteroid(self.position.x, self.position.y, new_asteroid_radius)
+            asteroid_one.velocity = new_direction * 1.2
+            asteroid_two.velocity = new_negative_direction * 1.2
